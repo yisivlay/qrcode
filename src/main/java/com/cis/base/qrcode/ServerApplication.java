41f1993dd07c0250d1e3d99d9aae03c1cd6a2679
DEV4Sep <mr.yisivlay@gmail.com>
2023-12-27 15:41:23 +0700

    init commit.

---
+++ src/main/java/com/cis/base/qrcode/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.cis.base.qrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cis/base/qrcode/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cis.base.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
