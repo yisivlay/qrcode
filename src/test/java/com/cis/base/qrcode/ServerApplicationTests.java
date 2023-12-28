dfa402d1ef1a8c25e0c90f11ed2e173269ce8048
DEV4Sep <mr.yisivlay@gmail.com>
2023-12-28 14:38:37 +0700

    updated gradle.

--- src/test/java/com/cis/base/qrcode/ServerApplicationTests.java
+++ src/test/java/com/cis/base/qrcode/ServerApplicationTests.java
@@ -2,6 +2,7 @@ package com.cis.base.qrcode;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 class ServerApplicationTests {
